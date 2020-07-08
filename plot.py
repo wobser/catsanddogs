d81bd10c51506926df2ebd3ab27169b82ac980f6
wobser <39001440+wobser@users.noreply.github.com>
2020-07-08 11:46:10 +0200

    Create plot.py

---
+++ plot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs_range = range(my_epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
